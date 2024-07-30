178be3be760a8e9a83f46b3cbd3b0039069a9964
Noxaar <aaron.j.kent1@gmail.com>
2024-07-30 15:46:23 +0800

    feat(deploy): update sequelize
    - update to use postgres URL

--- backend/app/src/models/index.js
+++ backend/app/src/models/index.js
@@ -2,19 +2,27 @@ import {dbConfig} from "../config/db.config.js"
 import { LoremIpsum } from "lorem-ipsum";
 
 import { Sequelize, DataTypes } from "sequelize";
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  port: dbConfig.port,
-  operatorsAliases: false,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+let sequelize;
+
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    port: dbConfig.port,
+    operatorsAliases: false,
+  
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  });
+}
 
 const Post = sequelize.define('Post',
   {
