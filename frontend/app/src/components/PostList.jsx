12f1fc2d2a7e8edd0bbf1dc0a0546661c83cf0e9
Noxaar <aaron.j.kent1@gmail.com>
2024-07-31 15:01:10 +0800

    feat(order): use id as key

--- frontend/app/src/components/PostList.jsx
+++ frontend/app/src/components/PostList.jsx
@@ -34,11 +34,10 @@ export const PostList = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  console.log(data.posts.length);
   return (
     <Stack gap={3} ref={ref}>
       {data.posts.map((post) => {
-        return <Post post={post} key={post.title} />;
+        return <Post post={post} key={post.id} />;
       })}
     </Stack>
   );
